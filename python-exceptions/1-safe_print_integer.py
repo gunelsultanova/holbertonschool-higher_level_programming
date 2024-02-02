9c739a7046a7516bfb0186e4d0221395af15f8fe
aelitaa <gunel.sultanova@outlook.com>
2024-02-02 05:38:31 -0800

    yn

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{}".format(value))
+        print("{:d}".format(value))
     except (ValueError, TypeError):
         return False
     else:
