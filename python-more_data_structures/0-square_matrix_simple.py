cc4c541ad2a925328fda8e7fb79455e3a94ec9eb
aelitaa <gunel.sultanova@outlook.com>
2024-02-02 02:38:08 -0800

    indent

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -5,4 +5,4 @@ def square_matrix_simple(matrix=[]):
     for i in range (len(matrix)):
         matrix_new[i] = list(map(lambda x: x**2, matrix[i]))
 
-        return (matrix_new)
+    return (matrix_new)
