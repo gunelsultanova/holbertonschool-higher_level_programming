2acd846577e20a0f062ea70445ca3a2284f24a4f
aelitaa <gunel.sultanova@outlook.com>
2024-02-02 02:35:37 -0800

    square

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    matrix_new = matrix.copy()
+
+    for i in range (len(matrix)):
+        matrix_new[i] = list(map(lambda x: x**2, matrix[i]))
+
+        return (matrix_new)
