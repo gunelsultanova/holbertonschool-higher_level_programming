46dcd9929ca1157886c49c3450148b950da9ca95
aelitaa <gunel.sultanova@outlook.com>
2024-02-04 01:57:03 -0800

    square

---
+++ python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module defines a Square class.
+
+This module contains a class named Square that represents a square object.
+"""
+
+
+class Square:
+    """Class Square that defines a square object.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+    def __init__(self, size):
+        """Initialize method that stores the size of the square.
+
+        Args:
+            param1 (int): size of the square
+        """
+        self.__size = size
