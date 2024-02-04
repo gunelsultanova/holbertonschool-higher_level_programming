542a5da43d157f59d30676a0fcaf5104305425c0
aelitaa <gunel.sultanova@outlook.com>
2024-02-04 02:01:01 -0800

    2

---
+++ python-classes/2-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This module defines a Square class.
+
+The Square class represents a square object defined by its size.
+"""
+
+
+class Square:
+    """A class that defines a square by its size.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+    def __init__(self, size=0):
+        """Method to initialize the square object.
+
+        Args:
+            size (int): The size of the square (default is 0).
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(size)
