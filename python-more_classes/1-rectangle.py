d31e65b37e1e4de813df28ec347407fbfcd4172a
aelitaa <gunel.sultanova@outlook.com>
2024-02-10 05:51:15 -0800

    file

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -7,37 +7,79 @@ This module is composed by a class that defines a Rectangle
 """
 
 
-"""Defines a Rectangle class."""
-
-
 class Rectangle:
-        """Represent a rectangle."""
+    """ Class that defines a rectangle """
+
     def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle.
+        """ Method that initializes the instance
 
         Args:
-            width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle.
-        """
-        self.__height = height
-        self.__width = width
+            width: width of the rectangle
+            height: height of the rectangle
 
+
+        """
+        self.width = width
+        self.height = height
+
+    @property
     def width(self):
-        """Get/set the width of the rectangle."""
+        """ method that returns the value of the width
+
+        Returns:
+            width of the rectangle
+
+
+        """
+
         return self.__width
 
+    @width.setter
     def width(self, value):
+        """ method that defines the width
+
+        Args:
+            value: width
+
+        Raises:
+            TypeError: if width is not an integer
+            ValueError: if width is less than zero
+
+
+        """
+
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    @property
     def height(self):
-        """Get/set the height of the rectangle."""
+        """ method that returns the value of the height
+
+        Returns:
+            height of the rectangle
+
+
+        """
+
         return self.__height
 
+    @height.setter
     def height(self, value):
+        """ method that defines the height
+
+        Args:
+            value: height
+
+        Raises:
+            TypeError: if height is not an integer
+            ValueError: if height is less than zero
+
+
+        """
+
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
