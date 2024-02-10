e601714bba6ea771fa21e48209d3f6b90ff3eb58
aelitaa <gunel.sultanova@outlook.com>
2024-02-10 05:43:36 -0800

    rectangle

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle"""
+        self.__height = height
+        self.__width = width
+
+    def width(self):
+        return self.__width
+
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def height(self):
+        return self.__height
+
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
