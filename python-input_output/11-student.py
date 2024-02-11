d28910448e2f58b40c97510a53685ef920842031
aelitaa <gunel.sultanova@outlook.com>
2024-02-11 07:32:03 -0800

    11

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -26,8 +26,8 @@ class Student:
         Args:
             attrs (list): (Optional) The attributes to represent.
         """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
+        if isinstance(attrs, list) and
+        all(isinstance(ele, str) for ele in attrs):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
 
