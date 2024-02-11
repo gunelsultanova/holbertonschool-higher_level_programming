56fd1a54f528aa713ab8ebca8c3bc17490c1e49c
aelitaa <gunel.sultanova@outlook.com>
2024-02-11 08:17:38 -0800

    2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -12,6 +12,6 @@ def is_same_class(obj, a_class):
         If obj is exactly an instance of a_class - True.
         Otherwise - False.
     """
-    if isinstance(obj, a_class):
-        return True
-    return False
+
+
+return type(obj) is a_class
