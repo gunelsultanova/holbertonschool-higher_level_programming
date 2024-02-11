7950a6c173317b7959e7585a878c51c428d252f0
aelitaa <gunel.sultanova@outlook.com>
2024-02-11 02:51:07 -0800

    0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Defines an object attribute lookup function."""
+
+
 def lookup(obj):
     """ Function that returns the list of available attributes
         and methods of an object
