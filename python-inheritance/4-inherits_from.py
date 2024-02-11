e0277e35c908670b14590c358d6c3db87798af89
aelitaa <gunel.sultanova@outlook.com>
2024-02-11 08:31:55 -0800

    revert

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -12,6 +12,6 @@ def inherits_from(obj, a_class):
         If obj is an inherited instance of a_class - True.
         Otherwise - False.
     """
-    if isinstance(obj, a_class) and not isinstance(obj, a_class):
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
